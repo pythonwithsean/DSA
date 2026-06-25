c00bb54b50b207d4902fa317be604a452abdd5ae
Sean <pythonwithsean@gmail.com>
2026-06-25 18:45:36 +0100

    Add toInt function to convert string to integer with error handling

---
+++ golang/str_int.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func toInt(s string) (int, error) {
+	res := 0
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if c < 0x30 || c > 0x39 {
+			return -1, fmt.Errorf("can not turn \"%c\" to int", c)
+		}
+		c -= byte('0')
+		res *= 10
+		res += int(c)
+	}
+	return res, nil
+}
+
+func main() {
+	x := "200"
+	fmt.Printf("%T %v\n", x, x)
+	p, err := toInt(x)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%T %v\n", p, p)
+}

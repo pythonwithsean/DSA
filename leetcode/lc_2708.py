771001bd6f27e3e4bfe0746abc74cda65965a087
Sean <pythonwithsean@gmail.com>
2026-06-28 19:14:54 +0100

    Refactor maxStrength function documentation and improve code clarity

--- leetcode/lc_2708.py
+++ leetcode/lc_2708.py
@@ -1,20 +1,15 @@
 from typing import List
+import math
 
 class Solution:
     def maxStrength(self, nums: List[int]) -> int:
         """
             - You are given a 0 indexed integer array nums
-
             - representing the score of the students in an exam
-
             - group of students with the maximal strength (none empty)
-
             - Where the strength of a group of students is nums[i] * ... nums[ik]
-
             return the maximum strength of a geroup the teacher can creater
-
             1 <= nums_length <= 13
-
             nums[i]
 
 
@@ -53,13 +48,24 @@ class Solution:
             if (i,amount) in memo:
                 return memo[(i,amount)]
             if amount == -math.inf:
+                # start as me as the current amount
                 take = dp(i+1,nums[i])
+                # start with the origina -math.inf
                 skip = dp(i+1,amount)
             else:
+                # start next with me * amount
                 take = dp(i +1,nums[i] * amount)
+                # start next without me just the amount
                 skip = dp(i + 1, amount)
             res = max(take,skip)
             memo[(i,amount)] = res
             return res
         res = dp(0,-math.inf)
         return res
+
+"""
+    This was a take it or leave it problem that required
+    2 states the current position and the amount i had at each position
+    at each position i needed to make sure i check if the amount i had was the default
+    if it was i needed to handle that correctly
+"""
